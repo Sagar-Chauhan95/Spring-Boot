9703d0f54f3637e36be12ebffdd6b1f46f1de8ef
Sagar Chauhan <schauhan@miu.edu>
2024-08-05 22:35:37 -0500

    spring security updated

---
+++ spring-Security/src/main/java/com/practice/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.practice.springsecurity.config;
+
+public class SecurityConfiguration {
+}
---
+++ spring-Security/src/main/java/com/practice/springsecurity/user/Role.java
@@ -0,0 +1,7 @@
+package com.practice.springsecurity.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
